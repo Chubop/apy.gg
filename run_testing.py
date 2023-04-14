a33ea7ff9d9e4751faab4325857ef3492b7b71e5
chubop <samuelokirby@gmail.com>
2023-04-14 11:40:14 -0400

    added data_vis.py to visualize failure rate

--- run_testing.py
+++ run_testing.py
@@ -1,4 +1,5 @@
 import asyncio, json, io
+import data_vis
 from contextlib import redirect_stdout
 from scraper import scrape_bank
 from pprint import pprint
@@ -8,6 +9,7 @@ from random import shuffle
 async def check_bank_scraper():
     whole = valids = 0
     failed_banks = []
+    tests = [] # tests passed in for data_vis
     print("Starting test.")
     file_name = 'banks.json'
     with open(file_name, 'r') as file:
@@ -27,12 +29,14 @@ async def check_bank_scraper():
             failed_banks.append( (name, url))
         whole += 1
         print('Current Accuracy:', valids, '/', whole, ',', float(valids/whole))
+        tests.append(float(valids/whole) * 100)
     print("===RESULTS===")
     print(str(valids) + '/' + str(whole), "tests passed.")
     print(str(float(valids / whole)) + '%', "success rate")
     if len(failed_banks) > 0:
         print("Failed Banks: ")
         pprint(failed_banks)
+    data_vis.map_scraper(tests)
 
 
 if __name__ == '__main__':
