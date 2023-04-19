5aa4930f0ee551971f525f45227967d1f78cbfc7
Chubop <samuelokirby@gmail.com>
2023-04-18 20:00:49 -0400

    citizens support, changed to dotmap

--- sign_ups.py
+++ sign_ups.py
@@ -3,6 +3,7 @@ import json
 from enum import Enum
 from playwright.async_api import async_playwright
 from us_state_abbreviations import convert_tag
+from dotmap import DotMap
 
 
 class SignUp:
@@ -28,7 +29,14 @@ class User:
 
 
 async def citizens_signup(info):
-    bank = SignUp("Citizens", "https://application.citizensaccess.com/openaccount/?productType=3000&productTerm=&openingAmount#!/acctopening")
+    def app_loading_hidden(page):
+        return page.evaluate("""() => {
+            const appLoadingElement = document.querySelector('#appLoading');
+            return appLoadingElement && appLoadingElement.style.display === 'none';
+        }""")
+
+    bank = SignUp("Citizens",
+                  "https://application.citizensaccess.com/openaccount/?productType=3000&productTerm=&openingAmount#!/acctopening")
     user = User(info)
     print("Are you a current Citizens Customer?")
     start_input = int(input(
@@ -53,121 +61,72 @@ async def citizens_signup(info):
         browser = await p.chromium.launch(headless=False)
         context = await browser.new_context()
         page = await context.new_page()
+
         await page.goto(bank.url)
+        time.sleep(5)
+        # Wait for the div element with class "dimBackground" to have display: none
+        await page.is_hidden('#appLoading')
 
         await page.fill("#amount0", "0.01")
 
+        if await page.is_visible('#appLoading'):
+            await page.is_hidden('#appLoading')
+            await page.fill("#amount0", "0.01")
+
         # Go to next page
         await page.wait_for_selector("button:text('Continue')")
         await page.click("button:text('Continue')")
+        # Wait for the div element with class "dimBackground" to have display: none
 
-        await page.wait_for_selector("#first-name")
-        await page.fill("#first-name", user.first_name)
-        await page.fill("#last-name", user.last_name)
-
-        # Go to next page
-        await page.click("button:text('Continue')")
-        await page.wait_for_selector("#email-address")
-
-        await page.fill("#email-address", user.contact_info['email'])
-        await page.fill("#confirm-email-address", user.contact_info['email'])
-
-        # Go to next page
-        await page.click("#button-4")
-        await page.wait_for_selector("#mobile-phone-number")
-
-        await page.fill("#mobile-phone-number", user.contact_info['phone_number'])
-
-        # Go to next page
-        await page.click("#button-6")
-        await page.wait_for_selector("#primary-street")
-
-        await page.fill('#primary-street', user.contact_info['address'])
-        # TODO: apt or suite option
-        await page.fill('#primary-city', user.contact_info['city'])
-        await page.select_option('#primary-state', user.contact_info['state'])
-        await page.fill("#primary-zip-code", user.contact_info['zip'])
+        await page.is_hidden('#appLoading')
+        time.sleep(5)
+        await page.fill("#firstName", user.first_name)
+        await page.fill('#lastName', user.last_name)
+        await page.fill('#zipCode2', user.contact_info.zip)
+        await page.fill('#city', user.contact_info.city)
+        await page.fill('#primary2', user.contact_info.address)
 
         if user.different_mailing_address:
-            await page.click("button:text('+ Add mailing address (if different from residential address)')")
-            await page.fill('#secondary-street', user.contact_info['address'])
-            # TODO: apt or suite option
-            await page.fill('#secondary-city', user.contact_info['city'])
-            await page.select_option('#secondary-state', user.contact_info['state'])
-            await page.fill("#secondary-zip-code", user.contact_info['zip'])
-        await page.click("button:text('Continue')")
+            await page.click("label[for='isMailingAddress']")
 
-        # Go to next page
+            await page.fill('#mailingZipCode2', user.mailing_info.zip)
+            await page.fill('#mailing2', user.mailing_info.address)
+            await page.fill('#phone', user.contact_info.phone_number)
+            await page.fill("input[name='mailingCity']", user.mailing_info.city)
+            await page.select_option('#isThisMobile', 'Mobile')
+            await page.fill('#email', user.contact_info.email)
+            # await page.select_option(convert_tag(user.mailing_info.state))
+
+        await page.fill('#dob', user.date_of_birth.replace('-', '/'))
+        # TODO: more occupation support
+        await page.select_option('#occupation', user.employment_info.occupation)
+        await page.evaluate("document.getElementById('caxssn').value = '{}'".format(user.ssn))
+        await page.type('#caxssn', user.ssn)
+        await page.click("button[cbdata-reason='personal-info-continue']")
+
+        # Next page
         await page.wait_for_selector("button:text('Continue')")
         await page.click("button:text('Continue')")
 
-        # TODO: specific employment status
-        if user.employment_info['employed']:
-            await page.click("#button-0")
-        else:
-            await page.click('#button-4')
+        await page.wait_for_selector("label[for='electronicCheck']")
+        await page.click("label[for='electronicCheck']")
 
-        # TODO: greater option support instead of hardcoded
-        await page.select_option("#job-title", "Engineer/Scientist")
+        await page.wait_for_selector("label[for='termsCheck']")
+        await page.click("label[for='termsCheck']")
 
-        # Go to next page
-        await page.wait_for_selector("button:text('Continue')")
-        await page.click("button:text('Continue')")
+        await page.wait_for_selector("label[for='usResident']")
+        await page.click("label[for='usResident']")
 
-        salary = int(user.employment_info['annual_income'])
-        if salary == 0:
-            await page.click("button:text('No income')")
-        elif salary <= 50000:
-            await page.click("button:text('$1 - $50,000')")
-        elif salary <= 100000:
-            await page.click("button:text('$50,000 - $100,000')")
-        elif salary <= 150000:
-            await page.click("button:text('$100,000 - $150,000')")
-        elif salary <= 250000:
-            await page.click("button:text('$150,000 - $250,000')")
-        else:
-            await page.click("button:text('Over $250,000')")
+        await page.wait_for_selector("select[name='sourceOfIncomeOpts']")
+        await page.select_option("select[name='sourceOfIncomeOpts']", user.employment_info.source_of_income)
 
-        await page.click("button:text('Continue')")
+        await page.select_option("select[name='multiAcctUsage']", "Long-Term Savings (Education, Home, etc.)")
 
-        # Go to next page
-        await page.wait_for_selector("#date-of-birth")
-        await page.fill("#date-of-birth", user.date_of_birth.replace("-", ""))
-        await page.fill("#ssn-or-itin", user.ssn)
-        await page.click("button:text('Continue')")
+        await page.select_option("select[name='isIntlTransSelected']", "No")
 
-        await page.wait_for_selector("p:text('I certify the following:')")
-        await page.click("p:text('I certify the following:')")
-        await page.click("button:text('Continue')")
-
-        await page.wait_for_selector("label[for='ods-radio-2-input']")
-        await page.click("label[for='ods-radio-2-input']")
-        await page.click("button:text('Continue')")
-
-        # Go to next page
-        await page.wait_for_selector("label[for='withholding-no-input']")
-        if user.tax_info['withholding']:
-            await page.click("label[for='withholding-yes-input']")
-        else:
-            await page.click("label[for='withholding-no-input']")
-        await page.click("label[for='ods-checkbox-6-input']")
-        await page.click("button:text('Continue')")
-
-        # Go to next page
-        await page.wait_for_selector("#fund-later-button")
-        await page.click("#fund-later-button")
-
-        # Go to next page
-        await page.wait_for_selector("#button-open-account")
-        await page.click("#button-open-account")
-
-        input('Account ready.')
-        # Go to next page, finish submit.
-        await page.wait_for_selector("#button-0")
-        await page.click("#button-0")
+        input("Account ready.")
 
         time.sleep(999)
-
 
 
 async def capital_one_signup(info):
@@ -216,32 +175,32 @@ async def capital_one_signup(info):
         await page.click("button:text('Continue')")
         await page.wait_for_selector("#email-address")
 
-        await page.fill("#email-address", user.contact_info['email'])
-        await page.fill("#confirm-email-address", user.contact_info['email'])
+        await page.fill("#email-address", user.contact_info.email)
+        await page.fill("#confirm-email-address", user.contact_info.email)
 
         # Go to next page
         await page.click("#button-4")
         await page.wait_for_selector("#mobile-phone-number")
 
-        await page.fill("#mobile-phone-number", user.contact_info['phone_number'])
+        await page.fill("#mobile-phone-number", user.contact_info.phone_number)
 
         # Go to next page
         await page.click("#button-6")
         await page.wait_for_selector("#primary-street")
 
-        await page.fill('#primary-street', user.contact_info['address'])
+        await page.fill('#primary-street', user.contact_info.address)
         # TODO: apt or suite option
-        await page.fill('#primary-city', user.contact_info['city'])
-        await page.select_option('#primary-state', user.contact_info['state'])
-        await page.fill("#primary-zip-code", user.contact_info['zip'])
+        await page.fill('#primary-city', user.contact_info.city)
+        await page.select_option('#primary-state', user.contact_info.state)
+        await page.fill("#primary-zip-code", user.contact_info.zip)
 
         if user.different_mailing_address:
             await page.click("button:text('+ Add mailing address (if different from residential address)')")
-            await page.fill('#secondary-street', user.contact_info['address'])
+            await page.fill('#secondary-street', user.contact_info.address)
             # TODO: apt or suite option
-            await page.fill('#secondary-city', user.contact_info['city'])
-            await page.select_option('#secondary-state', user.contact_info['state'])
-            await page.fill("#secondary-zip-code", user.contact_info['zip'])
+            await page.fill('#secondary-city', user.contact_info.city)
+            await page.select_option('#secondary-state', user.contact_info.state)
+            await page.fill("#secondary-zip-code", user.contact_info.zip)
         await page.click("button:text('Continue')")
 
         # Go to next page
@@ -249,7 +208,7 @@ async def capital_one_signup(info):
         await page.click("button:text('Continue')")
 
         # TODO: specific employment status
-        if user.employment_info['employed']:
+        if user.employment_info.employed:
             await page.click("#button-0")
         else:
             await page.click('#button-4')
@@ -261,7 +220,7 @@ async def capital_one_signup(info):
         await page.wait_for_selector("button:text('Continue')")
         await page.click("button:text('Continue')")
 
-        salary = int(user.employment_info['annual_income'])
+        salary = int(user.employment_info.annual_income)
         if salary == 0:
             await page.click("button:text('No income')")
         elif salary <= 50000:
@@ -293,7 +252,7 @@ async def capital_one_signup(info):
 
         # Go to next page
         await page.wait_for_selector("label[for='withholding-no-input']")
-        if user.tax_info['withholding']:
+        if user.tax_info.withholding:
             await page.click("label[for='withholding-yes-input']")
         else:
             await page.click("label[for='withholding-no-input']")
@@ -377,8 +336,8 @@ async def amex_signup(info):
         await page.click("button[id='submit-button']")
         await page.wait_for_selector("#email")
 
-        await page.fill('#email', user.contact_info['email'])
-        await page.fill('#phone', user.contact_info['phone_number'])
+        await page.fill('#email', user.contact_info.email)
+        await page.fill('#phone', user.contact_info.phone_number)
 
         if user.uses_paper:
             await page.click("label[for='ELECTRONIC_AND_PAPER']")
@@ -389,36 +348,36 @@ async def amex_signup(info):
         await page.click("button[id='submit-button']")
         await page.wait_for_selector("input[id='line1']")
 
-        await page.fill('#line1', user.contact_info['address'])  # street address
-        await page.fill('#city', user.contact_info['city'])  # street address
+        await page.fill('#line1', user.contact_info.address)  # street address
+        await page.fill('#city', user.contact_info.city)  # street address
         # TODO: appt, suite #
-        await page.select_option("#region", convert_tag(user.contact_info['state']))
-        await page.fill('#postalCode', user.contact_info['zip'])  # street address
+        await page.select_option("#region", convert_tag(user.contact_info.state))
+        await page.fill('#postalCode', user.contact_info.zip)  # street address
 
         if user.different_mailing_address:
             await page.click("label[for='differentMailingAddress']")
-            await page.fill('#mailingLine', user.mailing_info['address'])  # street address
+            await page.fill('#mailingLine', user.mailing_info.address)  # street address
             # TODO: appt, suite #
-            await page.fill('#mailingCity', user.mailing_info['city'])  # street address
-            await page.select_option("#mailingState", convert_tag(user.mailing_info['state']))
-            await page.fill('#mailingZip', user.mailing_info['zip'])  # street address
+            await page.fill('#mailingCity', user.mailing_info.city)  # street address
+            await page.select_option("#mailingState", convert_tag(user.mailing_info.state))
+            await page.fill('#mailingZip', user.mailing_info.zip)  # street address
 
         # Go to next page
         await page.click("button[id='submit-button']")
         await page.wait_for_selector("select[id='employmentStatus']")
 
-        if user.employment_info['employed']:
+        if user.employment_info.employed:
             await page.select_option('#employmentStatus', 'Employed')
-            await page.select_option('#occupation', user.employment_info['occupation'])
+            await page.select_option('#occupation', user.employment_info.occupation)
         else:
             await page.select_option('#employmentStatus', 'Unemployed')
-        await page.fill("#amount", user.employment_info['annual_income'])
+        await page.fill("#amount", user.employment_info.annual_income)
 
         # Go to next page
         await page.click("button[id='submit-button']")
         await page.wait_for_selector("label[for='noTaxWithholdings']", timeout=1000 * 999)
 
-        if user.tax_info['withholding']:
+        if user.tax_info.withholding:
             await page.click("label[for='haveTaxWithholdings']")
         else:
             await page.click("label[for='noTaxWithholdings']")
@@ -435,7 +394,7 @@ async def amex_signup(info):
 
 if __name__ == '__main__':
     with open('account_information.json', 'r') as outfile:
-        data = json.load(outfile)
+        data = DotMap(json.load(outfile))
     # asyncio.run(amex_signup(data))
-    # asyncio.run(capital_one_signup(data))
-    asyncio.run(citizens_signup(data))
+    asyncio.run(capital_one_signup(data))
+    # asyncio.run(citizens_signup(data))
